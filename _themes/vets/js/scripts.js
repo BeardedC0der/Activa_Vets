445208c8b4ceb89568cf9fba2a45bdb33cf30d40
Geoff Allman <geoff@netnak.co.uk>
2015-06-30 17:35:55 +0100

    Adding pictures

--- _themes/vets/js/scripts.js
+++ _themes/vets/js/scripts.js
@@ -1,3 +1,37 @@
+$(document).ready(function () {
+	closePopUp();
+	navtoggle();
+	selectNav();
+	
+});
 
+function navtoggle() {
+	$('.nav-toggle').click(function() {
+		$('.site-nav, .nav-toggle, body').toggleClass('site-nav--visible');
+		
+		if($('.nav-toggle').hasClass('site-nav--visible')) {
+			$('.nav-toggle__label').text('Close');
+		}
+		else {
+			$('.nav-toggle__label').text('Menu');
+		}
+	});
+	$('.site-nav a').click(function() {
+		$('.site-nav, .nav-toggle').removeClass('site-nav--visible');
+	});
+}
 
+function selectNav() {
+	$("nav select").change(function() {
+		window.location = $(this).find("option:selected").val();
+	});
+}
 
+function closePopUp() {
+	$('.user-details__close').click(function() {
+		$('.user-details').hide();
+		
+		// Unlocks body
+		$('body').removeClass('site-nav--visible');
+	});
+}
