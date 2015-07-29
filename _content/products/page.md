cdc2f821656f39cfb99b23bb2eb72b08687a4bed
Geoff <geoff@netnak.co.uk>
2015-07-29 16:56:10 +0100

    Debris oft page nearly done

--- _content/products/page.md
+++ _content/products/page.md
@@ -1,4 +1,3 @@
 ---
 title: Products
-_default_folder_template: product-slider
 ---
--- _themes/vets/js/scripts.js
+++ _themes/vets/js/scripts.js
@@ -1,43 +1,18 @@
 $(document).ready(function () {
-	closePopUp();
-	navtoggle();
-	selectNav();
-	
+	navToggle();
+	closeNav();
 });
+function navToggle(){
+	$('#toggle').click(function() {
+	   $(this).toggleClass('active');
+	   $('body').toggleClass('active');
+	   $('#overlay').toggleClass('open');
+	  });
+	}
 
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('body').toggleClass('active');
-   $('#overlay').toggleClass('open');
-  });
-
-function navtoggle() {
-	$('.nav-toggle').click(function() {
-		$('.site-nav, .nav-toggle, body').toggleClass('site-nav--visible');
-		
-		if($('.nav-toggle').hasClass('site-nav--visible')) {
-			$('.nav-toggle__label').text('Close');
-		}
-		else {
-			$('.nav-toggle__label').text('Menu');
-		}
-	});
-	$('.site-nav a').click(function() {
-		$('.site-nav, .nav-toggle').removeClass('site-nav--visible');
-	});
-}
-
-function selectNav() {
-	$("nav select").change(function() {
-		window.location = $(this).find("option:selected").val();
-	});
-}
-
-function closePopUp() {
-	$('.user-details__close').click(function() {
-		$('.user-details').hide();
-		
-		// Unlocks body
-		$('body').removeClass('site-nav--visible');
-	});
-}
+function closeNav(){
+	$('.overlay_menu ul li a').click(function() {
+		$('#toggle, body').removeClass('active');
+		$('#overlay').removeClass('open');
+		});
+	}
