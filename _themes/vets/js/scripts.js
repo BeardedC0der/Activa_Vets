9586d60a46e2e8ef9a2a8b95e430b08d43274e25
Geoff <geoff@netnak.co.uk>
2015-08-04 22:50:12 +0100

    Tuesday evening changes
    -tabs
    -wraps
    -typography

--- _themes/vets/js/scripts.js
+++ _themes/vets/js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
 	navToggle();
 	closeNav();
+	tabs();
+	accordionToggle();
 });
 function navToggle(){
 	$('#toggle').click(function() {
@@ -16,3 +18,35 @@ function closeNav(){
 		$('#overlay').removeClass('open');
 		});
 	}
+	
+	
+function tabs() {
+    
+    $( '.tab' ).click( function() {
+        var $tabclicked = $( this );
+            
+        //clean up
+        $( '.tab__content' ).hide();
+        $( '.tab' ).removeClass( 'tab--active' );
+            
+        //show new tab
+        $tabclicked.addClass( 'tab--active' );
+        $( $tabclicked.attr('href') ).show();
+
+        return false;
+    });
+        
+    //hide all the tabs panels
+    $( '.tab__content' ).hide();
+        
+    //click the first tab
+    $('.tab').first().click();
+    
+}
+
+function accordionToggle() {
+    $('.tab--mobile').click(function() {
+        $(this).next().toggleClass('tab__content--show');
+        return false;
+    });
+}
